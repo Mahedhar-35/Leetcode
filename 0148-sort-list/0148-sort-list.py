3cf7d2152092e6e5e81018021caa221521d96a3f
Mahedhar-35 <24cs01035@iitbbs.ac.in>
2026-05-29 23:29:19 +0530

    Time: 424 ms (71.01%), Space: 51.9 MB (9.35%) - LeetHub

--- 0148-sort-list/0148-sort-list.py
+++ 0148-sort-list/0148-sort-list.py
@@ -12,19 +12,15 @@ class Solution(object):
         
         current=head
         count=[]
-        if head ==None:
-            return head
         while current!=None:
             count.append(current.val)
             current=current.next 
         count=sorted(count)
         print(count)
-        result=ListNode()
-        dummy=result
+        current=head
         for i in range(len(count)):
-            dummy.next=ListNode(count[i])
-            dummy=dummy.next
-        dummy.next=None    
-        return result.next
+            current.val=count[i]
+            current=current.next  
+        return head
 
         
