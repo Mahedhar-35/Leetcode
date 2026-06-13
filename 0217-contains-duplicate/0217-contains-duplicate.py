13d3d4dffd9a187dc00252fd4379fb092f9bc772
Mahedhar-35 <24cs01035@iitbbs.ac.in>
2026-06-13 20:57:52 +0530

    Time: 18 ms (64.1%), Space: 26 MB (10.46%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        num=list(set(nums))
+        return len(nums)>len(num)
