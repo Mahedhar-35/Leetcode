b16ef0807494011a6977f25db053bf685898ae20
Mahedhar-35 <24cs01035@iitbbs.ac.in>
2026-06-19 10:28:06 +0530

    Time: 0 ms (100%), Space: 12.4 MB (13.82%) - LeetHub

---
+++ 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def countNumbersWithUniqueDigits(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        result=[1,10,91,739,5275,32491,168571,712891,2345851]
+        return result[n]
